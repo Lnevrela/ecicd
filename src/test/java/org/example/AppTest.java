1af7faf11c9b008c2d13835281f71eb1e36361ab
Lukas Nevrela <lukas_nevrela@epam.com>
2025-05-09 14:17:58 +0200

    Pipes test

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(5, Main.add(2, 3));
+    }
+}
